0e4425eb8d6779e23e5df97e650d343bd65559fe
daxi <aljedaxi@gmail.com>
2020-10-12 13:46:14 -0600

    link to vid didn't work

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,16 +1,29 @@
 import React from 'react';
 import { ButtonLink } from './Steps';
+import {Button} from '@material-ui/core';
 
 const containerStyle = {
 	display: 'flex', flexDirection: 'column', alignItems: 'center'
+};
+const buttonStyle = {color: 'black', textDecoration: 'none', background: 'white', width: 'max-content', margin: 10, display: 'grid', placeItems: 'center'};
+export const ButtonA = props => {
+	const {to, children} = props;
+	const style = Object.assign({}, buttonStyle, props.style);
+	return (
+		<div style={style}>
+			<a href={to}>
+				<Button> {children} </Button>
+			</a>
+		</div>
+	);
 };
 export const Home = props => {
 	const vid = "https://www.youtube.com/watch?v=AI4ynXzkSQo";
 	return (
 		<div style={containerStyle}>
-			<ButtonLink style={{width: 'auto'}} to={vid}>
+			<ButtonA style={{width: 'auto'}} to={vid}>
 				original video
-			</ButtonLink>
+			</ButtonA>
 			<ButtonLink to='/settings'>choose settings</ButtonLink>
 			<ButtonLink to='/step?step=1'>accept defaults</ButtonLink>
 		</div>
