6ee4e55354049e0d899548770ef28cd8d3a4910c
daxi <aljedaxi@gmail.com>
2020-10-12 21:33:27 -0600

    there's still a problem with floats and negative ints though

--- src/pages/Settings.jsx
+++ src/pages/Settings.jsx
@@ -1,12 +1,12 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import {waterToCoffee, coffeeToWater} from '../v60';
 import {Button, TextField, InputAdornment, Select, MenuItem} from '@material-ui/core';
 import { Link
 } from 'react-router-dom';
 
-const InputGrams = ({onChange, onBlur, value}) =>
+const InputGrams = props =>
 	<TextField
-		onChange={onChange} onBlur={onBlur} value={value}
+		{...props} type='number' autofocus
 		InputProps={{endAdornment: <InputAdornment position='end'>g</InputAdornment>}}
 	/>
 
@@ -14,28 +14,39 @@ export const Rules = props => {
 	const childStyle = {flex: '0 1 150px', margin: 5, display: 'flex', flexDirection: 'column', alignItems: 'center', background: 'white', color: 'black', padding: 10};
 	const parentStyle = {display: 'flex', flexWrap: 'wrap', justifyContent: 'center', width: '100%', height: '90%'};
 	const inputStyle = {width: '100%'};
-	const [water, setWater] = useState('0');
-	const [coffee, setCoffee] = useState(waterToCoffee(props.water ?? water));
+	const [water, setWater] = useState('');
+	const [coffee, setCoffee] = useState('');
 	const [roast, setRoast] = useState('light');
+	const [focusedField, setFocusedField] = useState();
+	const handleFocus = s => _ => 
+		s === 'water' ? setFocusedField('water') : setFocusedField('coffee');
+	const handleBlur = _ => setFocusedField(undefined);
+
+	useEffect(_ => {
+		if (focusedField === 'coffee') {
+			setWater(coffeeToWater(coffee));
+		} else if (focusedField === 'water') {
+			setCoffee(waterToCoffee(water));
+		}
+	}, [water, coffee])
 
 	const setty = s => e => s(e.target.value);
-
-	const handleBlur = isWater => _ => {
-		if(isWater) {
-			setCoffee(waterToCoffee(water));
-		} 
-		setWater(coffeeToWater(coffee));
-	};
 
 	return (
 		<div style={parentStyle}>
 			<div style={childStyle}>
 				water
-				<InputGrams value={water} onChange={setty(setWater)} onBlur={handleBlur(true)}/>
+				<InputGrams 
+					value={water} onChange={setty(setWater)} 
+					onBlur={handleBlur} onFocus={handleFocus('water')}
+				/>
 			</div>
 			<div style={childStyle}>
 				coffee
-				<InputGrams value={coffee} onChange={setty(setCoffee)} onBlur={handleBlur(false)}/>
+				<InputGrams 
+					value={coffee} onChange={setty(setCoffee)} 
+					onBlur={handleBlur} onFocus={handleFocus('coffee')}
+				/>
 				<div style={{height: 20}}/>
 				<Link to={`/step?step=1&water=${water}&roast=${roast}`}>
 					<Button tabIndex={-1} disabled={[0, '0'].includes(water)}>done</Button>
