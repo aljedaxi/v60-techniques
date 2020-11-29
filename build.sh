c071bae7224afb7a4d69acd423c78a57004ffdf3
jacob whitford-bender <jacob@xmentium.com>
2020-11-29 02:06:00 -0700

    added all command to build.sh

--- build.sh
+++ build.sh
@@ -1,11 +1,24 @@
-technique=$1
-echo "REACT_APP_TECHNIQUE=$technique" > .env.production
-cp $technique.ico public/favicon.ico
-yarn build
-pushd build
-case $technique in
-	"Eldric") echo "eldric-v60.surge.sh" > './CNAME';;
-	"Hoffmann") echo "hoffmann-v60.surge.sh" > './CNAME';;
-esac
-echo "\n" | surge
-popd
+buildApp() {
+	technique=$1
+	echo "REACT_APP_TECHNIQUE=$technique" > .env.production
+	cp $technique.ico public/favicon.ico
+	yarn build
+	pushd build
+	case $technique in
+		"Eldric") echo "eldric-v60.surge.sh" > './CNAME';;
+		"Hoffmann") echo "hoffmann-v60.surge.sh" > './CNAME';;
+	esac
+	echo "\n" | surge
+	popd
+}
+
+command=$1
+if [ $command = "all" ]
+then
+	for techne in Hoffmann Eldric
+	do
+		buildApp $techne
+	done
+else
+	buildApp $technique
+fi
